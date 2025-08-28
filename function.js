7b6279d7fc974b0e284ead8c8bcba5a715f5817b
ahhridoy01 <devilex.hridoy@gmail.com>
2025-08-28 23:16:18 +0600

    adding history add and clear functionality

--- function.js
+++ function.js
@@ -85,3 +85,63 @@ function copyText(numberId) {
 //   call history functionality
 
 
+const historyList = document.getElementById("history-list");
+const clearBtn = document.getElementById("clear-history");
+
+function addHistory(name, number) {
+    const time = new Date().toLocaleTimeString(); // current time
+
+    const li = document.createElement("li");
+    li.className = "flex justify-between items-center border-b pb-2";
+
+    li.innerHTML = `
+      <div>
+        <p class="font-semibold">${name}</p>
+        <p class="text-sm text-gray-500">${number}</p>
+      </div>
+      <span class="text-xs text-gray-400">${time}</span>
+    `;
+
+    historyList.appendChild(li);
+}
+
+document.getElementById("call1").addEventListener("click", function () {
+    addHistory("জাতীয় জরুরি সেবা", "999");
+});
+
+document.getElementById("call2").addEventListener("click", function () {
+    addHistory("পুলিশ", "999");
+});
+
+document.getElementById("call3").addEventListener("click", function () {
+    addHistory("ফায়ার সার্ভিস", "999");
+});
+
+document.getElementById("call4").addEventListener("click", function () {
+    addHistory("এম্বুলেন্স", "1994-999999");
+});
+
+document.getElementById("call5").addEventListener("click", function () {
+    addHistory("নারী ও শিশু সহায়তা", "109");
+});
+
+document.getElementById("call6").addEventListener("click", function () {
+    addHistory("দুদক", "106");
+});
+
+document.getElementById("call7").addEventListener("click", function () {
+    addHistory("বিদ্যুৎ বিভ্রাট", "16216");
+});
+
+document.getElementById("call8").addEventListener("click", function () {
+    addHistory("ব্র্যাক", "16445");
+});
+
+document.getElementById("call9").addEventListener("click", function () {
+    addHistory("বাংলাদেশ রেলওয়ে", "163");
+});
+
+clearBtn.addEventListener("click", function () {
+    historyList.innerHTML = "";
+});
+
